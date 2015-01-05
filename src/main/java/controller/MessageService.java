6f2cfbbbd7990328977d150115e3ba3c92670908
phelipot <phelipot@etud.insa-toulouse.fr>
2015-01-05 14:02:49 +0100

    Add the message service class

---
+++ src/main/java/controller/MessageService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import database.Consumer;
+import database.Provider;
+import database.Scenario;
+
+/**
+ *
+ * @author ender
+ */
+public class MessageService {
+    public void initConsumer(Consumer c)
+    {
+        
+    }
+    
+    public void initProvider(Provider p){
+        
+    }
+    
+    public void launch(Scenario s){
+        
+    }
+    
+    public void onEndOfProcess(){
+        
+    }
+}

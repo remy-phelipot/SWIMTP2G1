43efb08cdd78ad536b64702a8c6be786633feb1e
phelipot <phelipot@etud.insa-toulouse.fr>
2015-01-12 15:12:01 +0100

    Change Thread run method

--- src/main/java/controller/MessageListenerThread.java
+++ src/main/java/controller/MessageListenerThread.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
@@ -14,45 +7,38 @@ import javax.jms.JMSRuntimeException;
 import javax.jms.Queue;
 import javax.annotation.Resource;
 
-/**
- *
- * @author ender
- */
 public class MessageListenerThread extends Thread {
+
     private boolean communicationEnded = false;
 
     @Resource(lookup = "jms/myFactory")
     private static ConnectionFactory connectionFactory;
     @Resource(lookup = "jms/queue1")
     private static Queue queue;
-
-    private JMSContext context;
-    private JMSConsumer consumer;
+    
+    private final MessageService service;
 
     public MessageListenerThread(MessageService service) {
-        try {
-            context = connectionFactory.createContext();
-            consumer = context.createConsumer(queue);
-
-            // set the message listener
-            consumer.setMessageListener(service);
-
-        } catch (JMSRuntimeException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        this.service = service;
     }
 
     public void stopListening() {
         communicationEnded = true;
     }
-
+    
+    @Override
     public void run() {
-        while (!communicationEnded) {
-            try {
+        try (JMSContext context = connectionFactory.createContext();){
+            JMSConsumer consumer = context.createConsumer(queue);
+
+            // set the message listener
+            consumer.setMessageListener(service);
+
+            while (!communicationEnded) {
                 Thread.sleep(250);
-            } catch (InterruptedException ex) {
-                throw new RuntimeException(ex.getMessage());
             }
+        } catch (JMSRuntimeException | InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
